eb229c6dd79bc1457527326dcec05b0c55830313
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-04-01 18:04:01 +0900

    sample1&2 are okay

--- SushiReviews/SushiReviews.swift
+++ SushiReviews/SushiReviews.swift
@@ -83,41 +83,6 @@ func findPath(start: Int, originalStart: Int, mustVisit: [Int]) {
 
 }
 
-//var tempLength = 0
-//var alreadyPassed: Set<Int> = []
-//
-//func shortestPathBetweenTwo(a: Int, b: Int, numOfAll: Int) -> Int {
-//    alreadyPassed.insert(a)
-//    return helper(current: a, end: b, numOfAll: numOfAll)
-//}
-//
-//func helper(current: Int, end: Int, numOfAll: Int) -> Int {
-//    if alreadyPassed.count == numOfAll {
-//        return -1
-//    }
-//    if containsAll(array: paths[current], set: alreadyPassed) {
-//        return -1
-//    }
-//    for i in paths[current] {
-//        if alreadyPassed.contains(i) {
-//            continue
-//        }
-//        tempLength += 1
-//        alreadyPassed.insert(i)
-//        if i == end {
-//            return tempLength
-//        } else {
-//            if helper(current: i, end: end, numOfAll: numOfAll) == -1 {
-//                return -1
-//            } else {
-//                return helper(current: i, end: end, numOfAll: numOfAll)
-//            }
-//        }
-//    }
-//    return -1
-//}
-
-
 func containsAll(array: [Int], set: Set<Int>) -> Bool {
     for i in array {
         if !set.contains(i) {
@@ -125,4 +90,78 @@ func containsAll(array: [Int], set: Set<Int>) -> Bool {
         }
     }
     return true
+}
+
+func sushiReviews() {
+    struct Restaurant {
+        let number: Int
+        let adjacent: [Int]
+        let previousRestaurant: Int
+        let traveledRestaurants: Set<Int>
+        let travelTime: Int
+    }
+    
+    let firstLine = readLine()!.split(separator: " ").map { Int($0) }
+    let numOfN = firstLine[0]!
+    let numOfM = firstLine[1]!
+    let indexesOfM: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    var smallestTravelTime = 2 * numOfN
+    
+    var paths = [[Int]](repeating: [Int](repeating: 0, count: 0), count: numOfN)
+    
+    for _ in 1..<numOfN {
+        let pathInput = readLine()!.split(separator: " ").map { Int($0) }
+        paths[pathInput[0]!].append(pathInput[1]!)
+        paths[pathInput[1]!].append(pathInput[0]!)
+    }
+    
+    func findSmallestTravelTime(startRestaurant: Restaurant) {
+        let q = Queue<Restaurant>()
+        q.enqueue(item: startRestaurant)
+        var restaurantsToVisitDict: [Int: Bool] = [:]
+        for i in indexesOfM {
+            restaurantsToVisitDict[i] = false
+        }
+        
+        while !q.isEmpty() {
+            
+            let sq = q.dequeue()!
+            let number = sq.number
+            let adjacentRestaurants: [Int] = sq.adjacent
+            let previousReataurant: Int = sq.previousRestaurant
+            var traveledRestaurants: Set<Int> = sq.traveledRestaurants
+            let travelTime = sq.travelTime
+            traveledRestaurants.insert(number)
+            
+            if travelTime > smallestTravelTime {
+                break
+            }
+            if indexesOfM.contains(number) {
+                restaurantsToVisitDict[number] = true
+                if containsAll(array: indexesOfM, set: traveledRestaurants) {
+                    print("travelTime \(travelTime)")
+                    print(traveledRestaurants)
+                    if travelTime < smallestTravelTime {
+                        smallestTravelTime = travelTime
+                    }
+                    break
+                }
+            }
+            for i in 0..<adjacentRestaurants.count {
+                let adjacentRestaurantNumber = adjacentRestaurants[i]
+                if adjacentRestaurantNumber == previousReataurant && adjacentRestaurants.count > 1 {
+                    continue
+                }
+                q.enqueue(item: Restaurant(number: adjacentRestaurantNumber, adjacent: paths[adjacentRestaurantNumber], previousRestaurant: number, traveledRestaurants: traveledRestaurants, travelTime: travelTime + 1))
+            }
+        }
+    }
+    for restaurant in indexesOfM {
+        print("start \(restaurant)")
+        let emptySet: Set<Int> = []
+        findSmallestTravelTime(startRestaurant: Restaurant(number: restaurant, adjacent: paths[restaurant], previousRestaurant: -1, traveledRestaurants: emptySet, travelTime: 0))
+    }
+    print("output")
+    print(smallestTravelTime)
 }
--- SushiReviews/main.swift
+++ SushiReviews/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-sushi()
+//sushi()
+sushiReviews()
+
+/*
+ Sample Input 1
+8 2
+5 2
+0 1
+0 2
+2 3
+4 3
+6 1
+1 5
+7 3
+
+ Sample Output 1
+3
+ 
+ Sample Input 2
+8 5
+0 6 4 3 7
+0 1
+0 2
+2 3
+4 3
+6 1
+1 5
+7 3
+
+ Sample Output 2
+7
+ */
 
